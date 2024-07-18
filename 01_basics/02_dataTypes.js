c3090b4a15b6bef2845c8460a02211905836284a
ANSARI MOHAMMED SAQIB <86550147+mohammedsaqib1@users.noreply.github.com>
2024-07-18 10:45:15 +0530

    Update 02_dataTypes.js

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -5,25 +5,31 @@
 console.log(3 
     +
      3) // code readability should be high
-
+        // JS automatically avoids spaces
 console.log("Hitesh")
 
 
-let name = "hitesh"
-let age = 18
-let isLoggedIn = false
-let state;
+let name = "hitesh"        // string
+let age = 18               // number
+let isLoggedIn = false     // bool
+let state;                 // undefined
 
-// number => 2 to power 53
+// number(range) => 2 to power 53
 // bigint
-// string => ""
+// string => " " or ' '  (prefer " ")
 // boolean => true/false
 // null => standalone value
-// undefined => 
+// undefined => (value not assigned)
 // symbol => unique
 
 
 // object
 
+console.log(typeof "saqib");  // string
 console.log(typeof undefined); // undefined
-console.log(typeof null); // object
+console.log(typeof null); // object    
+
+/*
+--interview questions :
+    console.log(typeof null); // object  
+*/
