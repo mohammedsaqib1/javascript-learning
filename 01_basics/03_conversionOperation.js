8a810ea10fa2c2afa07071cce48872b42167afea
ANSARI MOHAMMED SAQIB <86550147+mohammedsaqib1@users.noreply.github.com>
2024-07-18 11:16:03 +0530

    Update 03_conversionOperation.js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,3 +1,4 @@
+/****Conversion of values using Number()****/
 let score = "hitesh"
 
 //console.log(typeof score);
@@ -8,24 +9,30 @@ let valueInNumber = Number(score)
 //console.log(valueInNumber);
 
 
-// "33" => 33
-// "33abc" => NaN
+// "33" => 33 
+// "33abc" => NaN 
 //  true => 1; false => 0
+//  NaN => NaN             // because console.log(typeof NaN) => number
+//  null => 0              //(dont use null because temp 0 deg.C is different in deg.F)
+//  undefined => NaN
 
+/****Conversion of values using Boolean()****/
 let isLoggedIn = "hitesh"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 //  console.log(booleanIsLoggedIn);
 
+
 // 1 => true; 0 => false
 // "" => false
 // "hitesh" => true
 
+/****Conversion of values using String()****/
 let someNumber = 33
 
 let stringNumber = String(someNumber)
 // console.log(stringNumber);
-// console.log(typeof stringNumber);
+// console.log(typeof stringNumber);  // resulting type is always "string"
 
 // *********************** Operations ***********************
 
